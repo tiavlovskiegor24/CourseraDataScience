bf78f312e3018502b01ba371ee78a69c53f2683b
tiavlovskiegor <tiavlovskiegor@gmail.com>
2016-05-26 00:15:57 +0400

    assignment 3 refresh

---
+++ GettingAndCleaningData/run_analysis.r
@@ -0,0 +1,58 @@
+library(plyr)
+
+## Load the data from files into R 
+xtest <- read.table("./GettindAndCleaningData/UCI HAR Dataset/test/X_test.txt")
+xtrain <- read.table("./GettindAndCleaningData/UCI HAR Dataset/train/X_train.txt")
+
+## Load the column names
+colnames <- read.table("./GettindAndCleaningData/UCI HAR Dataset/features.txt")#,stringsAsFactors = FALSE)
+
+## Assing the column names to data sets
+names(xtest) <- colnames$V2
+names(xtrain) <- colnames$V2
+
+## Load the activity labels and subject labels
+activitylabels <- read.table("./GettindAndCleaningData/UCI HAR Dataset/activity_labels.txt")#,stringsAsFactors = FALSE)
+testlabels <- read.table("./GettindAndCleaningData/UCI HAR Dataset/test/y_test.txt")#,stringsAsFactors = FALSE)
+trainlabels <- read.table("./GettindAndCleaningData/UCI HAR Dataset/train/y_train.txt")#,stringsAsFactors = FALSE)
+testsubjects <- read.table("./GettindAndCleaningData/UCI HAR Dataset/test/subject_test.txt",col.names = c("subject"))#,stringsAsFactors = FALSE)
+trainsubjects <- read.table("./GettindAndCleaningData/UCI HAR Dataset/train/subject_train.txt",col.names = c("subject"))#,stringsAsFactors = FALSE)
+
+## Assign activity names to lables
+testlabels$activity <- sapply(testlabels$V1,function(x) {tolower(activitylabels$V2[activitylabels$V1==x])}) 
+trainlabels$activity <- sapply(trainlabels$V1,function(x) {tolower(activitylabels$V2[activitylabels$V1==x])}) 
+
+## Creat category columns: test and training
+testcategories <- sample("test",size = nrow(xtest),replace = TRUE)
+traincategories <- sample("train",size = nrow(xtrain),replace = TRUE)
+
+## add the test/train,activities and subjects columns to the dataframes to uniquely identify each record
+xtest <- cbind(category = testcategories, subject=testsubjects, activity=testlabels$activity,xtest)
+xtrain <- cbind(category = traincategories,subject=trainsubjects,activity=trainlabels$activity,xtrain)
+
+## Merges the training and the test sets to create one data set
+mergeddata <- rbind(xtest,xtrain)
+
+## Extracts only the measurements on the mean and standard deviation for each measurement
+mergeddata <- mergeddata[,c(1:3,grep("[Mm]ean|[Ss]td",names(mergeddata)))]
+
+## Tidy the column names
+names(mergeddata) <- tolower(names(mergeddata))
+names(mergeddata) <- gsub("\\-","",names(mergeddata))
+names(mergeddata) <- gsub("\\()","",names(mergeddata))
+names(mergeddata) <- gsub("\\(","",names(mergeddata))
+names(mergeddata) <- gsub("\\)","",names(mergeddata))
+names(mergeddata) <- gsub("\\,","",names(mergeddata))
+
+##merge the test and training datasets
+mergeddata <- mergeddata[order(mergeddata$activity,mergeddata$subject),]
+
+##create and add a unique id for each record composing of category, activity and subject number
+id <- paste0(mergeddata$category,mergeddata$activity,mergeddata$subject)
+mergeddata <- cbind(id,mergeddata)
+
+##create a tidy data set with the average of each variable for each activity and each subject 
+averageddata <- ddply(mergeddata,.(id),function(x){ cbind(x[1,1:4],as.list(colMeans(x[,5:ncol(x)]))) })
+
+## order the data set
+averageddata <- averageddata[order(averageddata$activity,averageddata$subject),]
